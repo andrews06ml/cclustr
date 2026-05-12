1ecd711f9fa33da1f7b5fabd1a80d72e74cdd208
andrews06ml <andresfemole@gmail.com>
2026-05-12 09:37:01 -0500

    actualización

--- R/Imputation.R
+++ R/Imputation.R
@@ -1,3 +1,7 @@
+# Suppress R CMD check NOTE: '.imp' is used inside subset() in as_mild_list()
+# and is not a global variable but a column name passed as an unquoted symbol.
+utils::globalVariables(".imp")
+
 #' Standardize multiple imputation outputs into a unified list
 #'
 #' @description
